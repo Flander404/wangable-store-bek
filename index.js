a866fba1a175a10fc8fc2a2b3e218d0f4af8307f
Flander404 <109587664+Flander404@users.noreply.github.com>
2024-01-22 12:42:08 +0500

    connect witch bd

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use('/api', router)
+app.get('/', (req, res) => res.send('idex'))
 
 const start = async () => {
   try {
